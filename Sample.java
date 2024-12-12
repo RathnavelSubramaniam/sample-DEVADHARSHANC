7ba817827b3d0489fef1de905e1213c3de1f9b07
23BCADEVADHARSHAN <142072766+DEVADHARSHANC@users.noreply.github.com>
2024-12-12 09:46:16 +0000

    save

--- Sample.java
+++ Sample.java
@@ -3,14 +3,11 @@ public class Sample
 {
     public static void main(String args[])
     {
+         System.out.println("enter the nmber:");
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
-        String b=s.nextLine();
-        double c = s.nextDouble();
-        System.out.println("enter the nmber:");
         System.out.println("Interger value="+a);
-        System.out.println("type"+b);
-        System.out.println("value " + c);
+
     }
 
 }
