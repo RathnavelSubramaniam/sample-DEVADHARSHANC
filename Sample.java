782dd88647a7677586377ec0f5c3d697b71a826c
23BCADEVADHARSHAN <142072766+DEVADHARSHANC@users.noreply.github.com>
2024-12-12 09:42:38 +0000

    save

---
+++ Sample.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        String b=s.nextLine();
+        double c = s.nextDouble();
+        System.out.println("enter the nmber:");
+        System.out.println("Interger value="+a);
+        System.out.println("type"+b);
+        System.out.println("value " + c);
+    }
+
+}
